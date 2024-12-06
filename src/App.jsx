199bac8e97d2de32dfad993273b63ebf761ef65b
Eduard <occhipinti.eduard@icloud.com>
2024-12-06 05:21:53 +0100

    nuked server

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { Route, Routes } from "react-router-dom";
 import MessageContext from "./components/contexts/message/MessageContext.jsx";
-import MessageToast from "./components/contexts/message/MessageToast";
+import MessageToast from "./components/contexts/message/MessageToast.jsx";
 import useMessageContext from "./components/contexts/message/useMessageContext.mjs";
-import ErrorPage from "./components/errors/Error404Page";
-import ErrorBoundary from "./components/errors/ErrorBoundary";
-import Page from "./components/Page";
+import ErrorPage from "./components/errors/Error404Page.jsx";
+import ErrorBoundary from "./components/errors/ErrorBoundary.jsx";
+import Page from "./components/Page.jsx";
 
 /**
  * Main component of the application that manages the routing and the state of the user.
